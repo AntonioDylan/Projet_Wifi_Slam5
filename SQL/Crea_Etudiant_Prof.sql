ec345ea1f57a074575785d9e4ef8346505e646c7
Unknown <cascales.arthur@gmail.com>
2016-12-02 10:42:52 +0100

    requete creation sql

---
+++ SQL/Crea_Etudiant_Prof.sql
@@ -0,0 +1,23 @@
+CREATE TABLE  port_etudiant  (
+  num  int(11) NOT NULL AUTO_INCREMENT,
+  numGroupe  int(11) DEFAULT NULL,
+  nom  char(32) NOT NULL,
+  prenom  char(32) NOT NULL,
+  mel  char(64) NOT NULL,
+  mdp  char(32) NOT NULL,
+  numexam  char(16) DEFAULT NULL,
+  valide  char(1) NOT NULL DEFAULT 'O',
+ PRIMARY KEY ( num ),
+ KEY  ietudgrou  ( numGroupe )
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  port_professeur  (
+  num  int(11) NOT NULL AUTO_INCREMENT,
+  nom  char(32) NOT NULL,
+  prenom  char(32) NOT NULL,
+  mel  char(64) NOT NULL,
+  mdp  char(32) NOT NULL,
+  niveau  int(11) NOT NULL,
+  valide  char(1) DEFAULT 'O',
+ PRIMARY KEY ( num )
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
